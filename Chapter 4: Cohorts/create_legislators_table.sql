7ea2b22183c6fd178ccf6a46d42376fe4dbb004a
BAE Hyounjin <alexbaehj@gmail.com>
2026-01-09 16:49:24 +0900

    Update table

--- Chapter 4: Cohorts/create_legislators_table.sql
+++ Chapter 4: Cohorts/create_legislators_table.sql
@@ -1,42 +1,42 @@
-DROP table if exists legislators;
+-- Active: 1767840625700@@127.0.0.1@3306@sql_for_data_analysis
+DROP TABLE IF EXISTS legislators;
 
-CREATE table legislators
-(
-full_name varchar--name_official_full
-,first_name varchar --name_first
-,last_name varchar --name_last
-,middle_name varchar --name_middle
-,nickname varchar --name_nickname
-,suffix varchar --name_suffix
-,other_names_end date -- other_names_0_end date
-,other_names_middle varchar -- other_names_0_middle
-,other_names_last varchar -- other_names_0_last
-,birthday date -- bio_birthday
-,gender varchar-- bio_gender
-,id_bioguide varchar primary key
-,id_bioguide_previous_0 varchar
-,id_govtrack int
-,id_icpsr int
-,id_wikipedia varchar
-,id_wikidata varchar
-,id_google_entity_id varchar
-,id_house_history bigint
-,id_house_history_alternate int
-,id_thomas int
-,id_cspan int
-,id_votesmart int
-,id_lis varchar
-,id_ballotpedia varchar
-,id_opensecrets varchar
-,id_fec_0 varchar
-,id_fec_1 varchar
-,id_fec_2 varchar
-)
-;
+CREATE TABLE legislators (
+  full_name VARCHAR(255),
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  middle_name VARCHAR(100),
+  nickname VARCHAR(100),
+  suffix VARCHAR(50),
 
-COPY legislators
-FROM '/localpath/legislators.csv' -- change to the location you saved the csv file
-DELIMITER ','
-CSV HEADER
-;
+  other_names_end DATE,
+  other_names_middle VARCHAR(100),
+  other_names_last VARCHAR(100),
 
+  birthday DATE,
+  gender VARCHAR(20),
+
+  id_bioguide VARCHAR(20) PRIMARY KEY,
+  id_bioguide_previous_0 VARCHAR(20),
+
+  id_govtrack INT,
+  id_icpsr INT,
+
+  id_wikipedia VARCHAR(255),
+  id_wikidata VARCHAR(50),
+  id_google_entity_id VARCHAR(50),
+
+  id_house_history BIGINT,
+  id_house_history_alternate INT,
+  id_thomas INT,
+  id_cspan INT,
+  id_votesmart INT,
+
+  id_lis VARCHAR(20),
+  id_ballotpedia VARCHAR(255),
+  id_opensecrets VARCHAR(50),
+
+  id_fec_0 VARCHAR(20),
+  id_fec_1 VARCHAR(20),
+  id_fec_2 VARCHAR(20)
+);
